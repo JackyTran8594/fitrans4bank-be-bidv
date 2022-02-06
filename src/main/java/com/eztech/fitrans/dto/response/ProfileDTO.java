55938686a0521df4522a0c4d3b70e110b859334a
thuyetlv <lethuyet.10.11.1990@gmail.com>
2022-02-06 14:07:59 +0700

    update

--- src/main/java/com/eztech/fitrans/dto/response/ProfileDTO.java
+++ src/main/java/com/eztech/fitrans/dto/response/ProfileDTO.java
@@ -8,6 +8,7 @@ import com.eztech.fitrans.constants.ProfileTypeEnum;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.math.BigDecimal;
 
 import java.io.Serializable;
@@ -77,58 +78,70 @@ public class ProfileDTO implements Serializable {
     private Integer rate;
     private Boolean notifyByEmail;
 
-    // public ProfileDTO(Long id, Long customerid, Long staffId, Integer type,
-    //         Integer priority, LocalDateTime processDate,
-    //         String createdBy, LocalDateTime createdDate, String lastUpdatedBy, LocalDateTime lastUpdatedDate,
-    //         String status, Integer state, String cif, 
-    //         String customerName, 
-    //         LocalDateTime timeReceived_CM,
-    //         LocalDateTime timeReceived_CT,
-    //         LocalDateTime endTime,
-    //         Integer numberOfBill,
-    //         Integer numberOfPO,
-    //         String staffId_CM,
-    //         String staffId_CT,
-    //         String returnReason,
-    //         BigDecimal value,
-    //         String staffName, 
-    //         Integer rate) {
-    //     this.id = id;
-    //     this.customerid = customerid;
-    //     this.cif = cif;
-    //     this.customerName = customerName;
-    //     this.type = type;
-    //     if (type != null) {
-    //         this.typeEnum = ProfileTypeEnum.of(type).getName();
-    //     }
-    //     this.priority = priority;
-    //     if (priority != null) {
-    //         this.priorityValue = ProfilePriorityEnum.of(priority);
-    //     }
-    //     this.state = state;
-    //     if (state != null) {
-    //         this.stateEnum = ProfileStateEnum.of(state).getName();
-    //     }
-    //     this.staffId = staffId;
-    //     this.staffName = staffName;
-    //     this.processDate = processDate;
-    //     this.createdBy = createdBy;
-    //     this.createdDate = createdDate;
-    //     this.lastUpdatedBy = lastUpdatedBy;
-    //     this.lastUpdatedDate = lastUpdatedDate;
-    //     this.status = status;
-    //     this.rate = rate;
-    //     this.notifyByEmail = false;
-    //     this.timeReceived_CM = timeReceived_CM;
-    //     this.timeReceived_CT = timeReceived_CT;
-    //     this.endTime = endTime;
-    //     this.numberOfBill = numberOfBill;
-    //     this.numberOfPO = numberOfPO;
-    //     this.staffId_CM = staffId_CM;
-    //     this.staffId_CT = staffId_CT;
-    //     this.returnReason = returnReason;
-    //     this.value = value;
-    // }
+    //p.id,p.customer_id,p.staff_id,p.type,p.priority,p.process_date,p.time_received_ct, p.time_received_cm, p.end_time,
+// p.staff_id_cm, p.staff_id_ct, p.number_of_bill, p.number_of_po, p.value, p.return_reason, p.created_by,p.created_date,p.last_updated_by,p.last_updated_date,
+// p.status,p.state,p.rate, p.notify_by_email,c.cif,c.name as customer_name, s.name as staff_name
+
+    public ProfileDTO(Long id,
+                      Long customerid,
+                      Long staffId,
+                      Integer type,
+                      Integer priority,
+                      LocalDateTime processDate,
+                      LocalDateTime timeReceived_CT,
+                      LocalDateTime timeReceived_CM,
+                      LocalDateTime endTime,
+                      String staffId_CM,
+                      String staffId_CT,
+                      Integer numberOfBill,
+                      Integer numberOfPO,
+                      BigDecimal value,
+                      String returnReason,
+                      String createdBy, LocalDateTime createdDate, String lastUpdatedBy, LocalDateTime lastUpdatedDate,
+                      String status,
+                      Integer state,
+                      Integer rate,
+                      Boolean notifyByEmail,
+                      String cif,
+                      String customerName,
+                      String staffName
+                      ) {
+        this.id = id;
+        this.customerid = customerid;
+        this.cif = cif;
+        this.customerName = customerName;
+        this.type = type;
+        if (type != null) {
+            this.typeEnum = ProfileTypeEnum.of(type).getName();
+        }
+        this.priority = priority;
+        if (priority != null) {
+            this.priorityValue = ProfilePriorityEnum.of(priority);
+        }
+        this.state = state;
+        if (state != null) {
+            this.stateEnum = ProfileStateEnum.of(state).getName();
+        }
+        this.staffId = staffId;
+        this.staffName = staffName;
+        this.processDate = processDate;
+        this.createdBy = createdBy;
+        this.createdDate = createdDate;
+        this.lastUpdatedBy = lastUpdatedBy;
+        this.lastUpdatedDate = lastUpdatedDate;
+        this.status = status;
+        this.rate = rate;
+        this.timeReceived_CM = timeReceived_CM;
+        this.timeReceived_CT = timeReceived_CT;
+        this.endTime = endTime;
+        this.numberOfBill = numberOfBill;
+        this.numberOfPO = numberOfPO;
+        this.staffId_CM = staffId_CM;
+        this.staffId_CT = staffId_CT;
+        this.returnReason = returnReason;
+        this.value = value;
+        this.notifyByEmail = notifyByEmail;
+    }
 
     public void fillTransient() {
         if (priority != null) {
