8e7087ce2fe65429b18cc22bd40884137ebb1de2
unknown <bachtuvu.dev@gmail.com>
2022-03-28 00:27:33 +0700

    update grandauthorities for admin

--- src/main/java/com/eztech/fitrans/config/LdapUserAuthoritiesPopulator.java
+++ src/main/java/com/eztech/fitrans/config/LdapUserAuthoritiesPopulator.java
@@ -2,13 +2,18 @@ package com.eztech.fitrans.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collection;
+
+import com.eztech.fitrans.model.Role;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,7 +23,13 @@ public class LdapUserAuthoritiesPopulator implements LdapAuthoritiesPopulator {
 	private final UserDetailsService userDetailsService;
 
 	@Override
-	public Collection<? extends GrantedAuthority> getGrantedAuthorities(DirContextOperations userData, String username) {
-		return userDetailsService.loadUserByUsername(username).getAuthorities();
+	public Collection<? extends GrantedAuthority> getGrantedAuthorities(DirContextOperations userData,
+			String username) {
+		if (username.equals("admin")) {
+			return Arrays.asList(new SimpleGrantedAuthority(Role.ROLE_ADMIN));
+		} else {
+			return userDetailsService.loadUserByUsername(username).getAuthorities();
+		}
+
 	}
 }
