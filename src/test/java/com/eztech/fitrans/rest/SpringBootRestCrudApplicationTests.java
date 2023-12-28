43448e02c8aa75c4291f248ad2c460f3a6c5acba
JackyTran8594 <41407396+JackyTran8594@users.noreply.github.com>
2023-12-28 17:49:13 +0700

    test

--- src/test/java/com/eztech/fitrans/rest/SpringBootRestCrudApplicationTests.java
+++ src/test/java/com/eztech/fitrans/rest/SpringBootRestCrudApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.javachinna.rest;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class SpringBootRestCrudApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.javachinna.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
